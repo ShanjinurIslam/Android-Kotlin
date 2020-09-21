d498a128e646f1e9f3a5364263dfb9eed0880833
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-09-22 00:21:02 +0600

    Dice Roller with Image Resource Done!

---
+++ ImageResourcesCompatibility/app/src/main/java/com/shanjinur/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.shanjinur.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private val resourceMap = mapOf<Int,Int>(1 to R.drawable.dice_1, 2 to R.drawable.dice_2,3 to R.drawable.dice_3,4 to R.drawable.dice_4,5 to R.drawable.dice_5,6 to R.drawable.dice_6)
+    private lateinit var diceImage:ImageView
+    /*
+
+    val drawableResource = when (randomInt) {
+       1 -> R.drawable.dice_1
+       2 -> R.drawable.dice_2
+       3 -> R.drawable.dice_3
+       4 -> R.drawable.dice_4
+       5 -> R.drawable.dice_5
+       else -> R.drawable.dice_6
+    }
+
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val rollButton:Button = findViewById(R.id.rollButton)
+        diceImage = findViewById(R.id.diceImage)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        resourceMap[randomInt]?.let { diceImage.setImageResource(it) };
+    }
+}
